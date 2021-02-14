28b7857a6bd629af86a303c9593c95f68e4c3972
Ilgın <52226386+ilginavci@users.noreply.github.com>
2021-02-14 17:53:45 +0300

    son level

---
+++ gameJam/Assets/LastLevel.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LastLevel : MonoBehaviour
+{
+    public GameObject oldMan, player, effect ;
+    public float oldManHealt, TabutHealt;
+    float cooldown = 3f;
+    Animator oldManAnim, effectAnim;
+    float nextAttack=3f;
+    private void Start()
+    {
+        effectAnim = effect.GetComponent<Animator>();
+        oldManAnim = oldMan.GetComponent<Animator>();
+    }
+    private void Update()
+    { if(nextAttack <= Time.timeSinceLevelLoad)
+        {
+            nextAttack = Time.timeSinceLevelLoad + cooldown;
+            Attack();
+        }
+        
+    }
+    void Attack()
+    {
+        oldManAnim.SetTrigger("attack");
+        effectAnim.SetTrigger("attack");
+        TabutHealt -=50;
+        if (TabutHealt <= 0)
+        {
+            //kaybettik
+
+        }
+    }
+    public void getDamage()
+    {
+        oldManHealt -= 100;
+        if( oldManHealt <= 0)
+        {
+            //kazandık
+        }
+    }
+}
